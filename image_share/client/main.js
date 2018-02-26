555d1e8efcc0318b2de558f7499c475d2c7705d7
Anders12345 <andersskatan@hotmail.com>
2018-02-26 11:43:22 +0100

    Week 2 - Add an image rating function: Updating and sorting

--- image_share/client/main.js
+++ image_share/client/main.js
@@ -5,7 +5,10 @@ import './main.html';
 const Images = new Mongo.Collection('images');
 
 //Template.images.helpers({images:img_data});
-Template.images.helpers({images:Images.find()});
+Template.images.helpers({images:
+  Images.find({},{sort:{rating:-1}})
+
+});
 
 Template.images.events({
   'click .js-image': function(event) {
@@ -17,7 +20,13 @@ Template.images.events({
     $("#" + image_id).hide('slow', function() {
       Images.remove({"_id":image_id});
     });
-
+  },
+  'click .js-rate-image': function(event) {
+    var rating = $(event.currentTarget).data("userrating");
+    console.log(rating);
+    var image_id = this.id;
+    console.log(image_id);
+    Images.update({_id:image_id}, {$set: {rating:rating}});
   }
 });
 
