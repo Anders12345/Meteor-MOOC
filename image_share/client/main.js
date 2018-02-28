9332f246f3d93f5e1882abf1b5297ea24940d8af
Anders12345 <andersskatan@hotmail.com>
2018-02-28 16:35:44 +0100

    Infinite scroll

--- image_share/client/main.js
+++ image_share/client/main.js
@@ -4,6 +4,24 @@ import './main.html';
 
 const Images = new Mongo.Collection('images');
 
+Session.set("imageLimit", 8);
+
+lastScrollTop = 0;
+$(window).scroll(function(event){
+  //test if we are near the bottom of the window
+  if($(window).scrollTop() + $(window).height() > $(document).height() - 100){
+    // where are we in the page?
+    var scrollTop = $(this).scrollTop();
+    // see if we are going down or up
+    if(scrollTop > lastScrollTop) {
+      // yes we are heading down
+      Session.set("imageLimit", Session.get("imageLimit") + 4);
+    }
+    lastScrollTop = scrollTop;
+  }
+
+});
+
 Accounts.ui.config({
   passwordSignupFields: "USERNAME_AND_EMAIL"
 });
@@ -13,7 +31,7 @@ Template.images.helpers({
     if(Session.get("userFilter")){// they set a filter!
       return Images.find({createdBy:Session.get("userFilter")}, {sort:{createdOn: -1, rating:-1}});
     } else {
-      return Images.find({}, {sort:{createdOn: -1, rating:-1}});
+      return Images.find({}, {sort:{createdOn: -1, rating:-1}, limit:Session.get("imageLimit")});
     }
   },
   filtering_images:function() {
