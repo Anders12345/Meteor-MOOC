397053252b3f127d87b1a63eeb7b1d6bb3905d2e
Anders12345 <andersskatan@hotmail.com>
2018-02-28 14:45:52 +0100

    User authentication with Meteor.js

--- image_share/client/main.js
+++ image_share/client/main.js
@@ -48,22 +48,3 @@ Template.image_add_form.events({
     return false;
   }
 });
-/*
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
-*/
